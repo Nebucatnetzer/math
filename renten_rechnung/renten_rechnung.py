82618b067353fe90925cbacd3c5db4961fd5280f
Andreas Zweili <andreas@2li.ch>
2016-05-28 10:24:55 +0200

    Refractoring the code

--- renten_rechnung/renten_rechnung.py
+++ renten_rechnung/renten_rechnung.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 # This is the calculation the function is based on.
-#Kn=Ko*q^n+r*((q^n-1)/(q-1))
-#Kn= calculated_capital
-#Ko= start_capital
-#n= period_in_months
-#q= compounding_factor = 1+((z/100)/12)=1+((1.2%/100)/12)=1.001
-#r= monthly payment rate
-#z= interest
+# Kn=Ko*q^n+r*((q^n-1)/(q-1))
+# Kn= calculated_capital
+# Ko= start_capital
+# n= period_in_months
+# q= compounding_factor = 1+((z/100)/12)=1+((1.2%/100)/12)=1.001
+# r= monthly payment rate
+# z= interest
 
 # global variable to lock the choose_values function after it ran once
 values_chosen = False
+
 
 # function to enter the values to calculate with
 def choose_values():
@@ -21,37 +22,42 @@ def choose_values():
     values.rate = float(input("Wie hoch ist die monatliche Rate? "))
     values_chosen = True
 
+
 # function to calculate the compounding factor from the interest
 def calculate_compounding_factor():
-    #print("Calculating the compounding factor")
+    # print("Calculating the compounding factor")
     values.compounding_factor = 1 + ((values.interest / 100) / 12)
-    #print(values.compounding_factor)
+    # print(values.compounding_factor)
+
 
 # convert period_in_years variable into months
 def years_to_months():
-    #print("Calculating years into months")
+    # print("Calculating years into months")
     values.period_in_months = values.period_in_years * 12
-    #print(values.period_in_months)
+    # print(values.period_in_months)
+
 
 # the main pension formula in python
 def calculate_pension():
-    #print("Calculating Pension")
-    values.calculated_capital = (values.start_capital * 
-    values.compounding_factor ** values.period_in_months + values.rate * 
-    ((values.compounding_factor ** values.period_in_months - 1) / 
-    (values.compounding_factor - 1)))
-    #print(values.calculated_capital)
+    # print("Calculating Pension")
+    values.calculated_capital = (values.start_capital *
+                                 values.compounding_factor ** values.period_in_months + values.rate *
+                                 ((values.compounding_factor ** values.period_in_months - 1) /
+                                  (values.compounding_factor - 1)))
+    # print(values.calculated_capital)
+
 
 # function to increase the interest by a small amount
 def increase_interest():
-    #print("Increasing interest")
+    # print("Increasing interest")
     values.interest += 0.000001
-    #print(values.interest)
-    
+    # print(values.interest)
+
+
 # main class
 class Calculation(object):
     def __init__(self, interest, desired_capital, start_capital, period_in_years,
-                period_in_months, rate, compounding_factor, calculated_capital):
+                 period_in_months, rate, compounding_factor, calculated_capital):
         self.interest = interest
         self.desired_capital = desired_capital
         self.start_capital = start_capital
@@ -61,10 +67,12 @@ class Calculation(object):
         self.compounding_factor = compounding_factor
         self.calculated_capital = calculated_capital
 
+
 # initializing the values
-values = Calculation(interest=0.0001, desired_capital=1, start_capital=0, 
-        period_in_years=0, period_in_months=0, rate=0, 
-        compounding_factor=0, calculated_capital=0) 
+values = Calculation(interest=0.0001, desired_capital=1, start_capital=0,
+                     period_in_years=0, period_in_months=0, rate=0,
+                     compounding_factor=0, calculated_capital=0)
+
 
 # Beginning of the main function
 # if the calculated capital is smaller than the desired capital it will
@@ -81,8 +89,9 @@ def main():
         increase_interest()
         calculate_compounding_factor()
         calculate_pension()
-    print("Bei einem Zins von ",values.interest," erhalten sie nach ",
-            values.period_in_years," Jahren")
-    print(values.calculated_capital," Franken")
+    print("Bei einem Zins von ", values.interest, " erhalten sie nach ",
+          values.period_in_years, " Jahren")
+    print(values.calculated_capital, " Franken")
+
 
 if __name__ == "__main__": main()
