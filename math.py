5dde9e8f86bde3c0aa1bba9dcbe7d53282279c6e
Andreas <andreas@2li.ch>
2016-10-31 07:35:03 +0100

    create a module for the pension calculation

--- math.py
+++ math.py
@@ -1,9 +1,8 @@
-import pension_calculation
-from pension_calculation import calculation 
+import pension.calculate
 while True:
     print("What would you like to calculate?")
     print("1: Pension Calculation")
     chosen_calculation = int(input("Choose the desired calculation:"))
     if chosen_calculation == 1:
-            calculation.calculate_interest()
+            pension.calculate.interest()
 print("test")
